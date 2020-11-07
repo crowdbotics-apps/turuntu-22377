5001b52b53a8ce4f39f41c5bfec6d2cb262488d9
Crowdbotics <team@crowdbotics.com>
2020-11-07 08:00:45 +0000

    Update models

--- backend/turuntu_22377/wsgi.py
+++ backend/turuntu_22377/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turuntu_22377.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turuntu_22377.settings")
 
 application = get_wsgi_application()
